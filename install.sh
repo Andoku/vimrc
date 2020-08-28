7c7dd55d0a96646baf6e4ed8e7611993dacf88c6
Anton Tatuev <tatuev.a.a@yandex.ru>
2020-08-28 13:24:41 +0300

    исправил пути установки

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ echo ".vimrc patch complete.\n"
 
 # Install additional plugins
 echo "Installing linediff plugin.."
-git clone https://github.com/AndrewRadev/linediff.vim.git $HOME/.vim_runtime/my_plugins
+git clone https://github.com/AndrewRadev/linediff.vim.git $HOME/.vim_runtime/my_plugins/linediff.vim
 echo "Installing vim-cpp-modern plugin.."
-git clone https://github.com/bfrg/vim-cpp-modern.git $HOME/.vim_runtime/my_plugins
+git clone https://github.com/bfrg/vim-cpp-modern.git $HOME/.vim_runtime/my_plugins/vim-cpp-modern
 echo "Additional plugins installed successfully.\n"
 
 echo "Settings have been applied successfully."
